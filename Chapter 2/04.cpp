8fb976d396bce7f7cf9935ad26f33f64643048c3
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-02-03 23:26:31 +0300

    Update 04.cpp

--- Chapter 2/04.cpp
+++ Chapter 2/04.cpp
@@ -1,1 +1,20 @@
+//02_04ex.cpp
+//Напишите программу, выводящую на экран ваше любимое стихотворение.
+//Для разбиения на строчки используйте подходящую управляющую последовательность.
+
+#include <io.h> 
+#include <iostream>
+using namespace std;
+
+int main()
+{setlocale(LC_ALL, ".1251"); system("color 0F"); srand(time(NULL));
+	
+char line = '\n';
+cout << "У лукоморья дуб зеленый" <<line;
+cout << "Златая цепь на дубе том" << line;
+cout << "И днём и ночью кот учёный" << line;
+cout << "Всё ходит по цепи кругом" << line;
+
+system("pause"); return 0;
+}
 

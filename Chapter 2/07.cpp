83f59e073242a0f7a63e1d356b076215fd7f8deb
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-02-04 00:00:16 +0300

    Update 07.cpp

--- Chapter 2/07.cpp
+++ Chapter 2/07.cpp
@@ -4,17 +4,13 @@
 
 #include <iostream>
 using namespace std;
-int main(int argc, char* argv[])
+int main()
 {setlocale(LC_ALL, ".1251"); system("color 0F"); srand(time(NULL));
-	/*cout << "argc = " << argc << endl; for (int j = 0; j < argc; j++) cout << " Аргумент " << j << " = " << argv[j] << endl<<endl;*/
-	/*double c = circumf(5); cout << "c=" << c << endl;*/
 
 float t = 0.0;
 cout << "Введите температу по Цельсию:";
 cin >> t;
 cout << "Температура по Фаренгейту=" << t * 9 / 5 + 32<<endl; 
 	
-
-
 system("pause"); return 0;
 }

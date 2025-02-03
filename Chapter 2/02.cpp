7d99b1f61ebb082b68f58ba7ca99b41c22909eb9
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-02-03 22:52:19 +0300

    Update 02.cpp

--- Chapter 2/02.cpp
+++ Chapter 2/02.cpp
@@ -1,1 +1,21 @@
+//02_02ex.cpp
+//Напишите программу, выводящую следующую таблицу:
+//1990    135
+//1991   7290
+//1992  11300
+//1993  16200
 
+#include <io.h>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{setlocale(LC_ALL, ".1251"); system("color 0F"); srand(time(NULL));
+ 
+cout <<1990<<setw(8)<<135<<endl
+  << 1991<<setw(8)<<7290<<endl 
+  << 1992<<setw(8)<<11300<<endl
+  << 1993<<setw(8)<<16200<<endl;
+
+system("pause"); return 0;
+}

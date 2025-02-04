c0b6ad364c1829e48102a5169efa3c3d6e1d83f6
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-02-04 23:59:51 +0300

    Update 06.cpp

--- Chapter 4/06.cpp
+++ Chapter 4/06.cpp
@@ -22,25 +22,25 @@ char ch;
 cout << "Введите первую букву должности (laborer, secretary, manager, accountant, executive, researcher): "; 
 cin >> ch;
 cout << "Полное название должности: ";
-		switch (ch) 
- 		  	 {
-			case 'l': x = laborer; break;
-			case 's': x = secretary; break;
-			case 'm': x = manager; break;
-			case 'a': x = accountant; break;
-			case 'e': x = executive; break;
-			case 'r': x = researcher; break; 
-  			  }
+	switch (ch) 
+ 	 {
+		case 'l': x = laborer; break;
+		case 's': x = secretary; break;
+		case 'm': x = manager; break;
+		case 'a': x = accountant; break;
+		case 'e': x = executive; break;
+		case 'r': x = researcher; break; 
+  	  }
 
-		switch (x) 
-   			 {
-			case laborer: cout << "laborer"; break;
-			case secretary: cout << "secretary"; break;
-			case manager: cout << "manager"; break;
-			case accountant: cout << "accountant"; break;
-			case executive: cout << "executive"; break;
-			case researcher: cout << "researcher"; break; 
-   			 }
+	switch (x) 
+   	 {
+		case laborer: cout << "laborer"; break;
+		case secretary: cout << "secretary"; break;
+		case manager: cout << "manager"; break;
+		case accountant: cout << "accountant"; break;
+		case executive: cout << "executive"; break;
+		case researcher: cout << "researcher"; break; 
+   	 }
 cout<<endl; 
 system("pause"); return 0;
 }

646b9eb143f7c7365a47e527fb2e258ebfc9bf7a
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-02-04 23:39:58 +0300

    Update 05.cpp

--- Chapter 4/05.cpp
+++ Chapter 4/05.cpp
@@ -1,1 +1,23 @@
+/*04_05ex.cpp
+Создайте структуру типа date, содержащую три поля int: месяц, день и год. Попросите пользователя ввести день, месяц и год в формате 31/12/2002,
+сохраните введенное значение в структурной перменной, а затем извлеките данные из этой переменной и выведите их на экран в том же формате, в каком они вводились. */
 
+#include <iostream>
+using namespace std;
+
+struct date 
+{ 
+  int day, mounth, year; 
+};
+
+int main()
+{setlocale(LC_ALL, ".1251"); system("color 0F"); srand(time(NULL));
+ 
+date data; 
+char ch;
+cout << "Введите дату: "; 
+cin >> data.day >> ch >> data.mounth >> ch >> data.year;
+cout << "\nВы ввели дату:" << data.day << ch << data.mounth << ch << data.year << endl;
+	
+system("pause"); return 0;
+}

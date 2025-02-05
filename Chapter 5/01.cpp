30411a49070353c82d8d2647e94c0b4f5219933e
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-02-05 22:24:25 +0300

    Update 01.cpp

--- Chapter 5/01.cpp
+++ Chapter 5/01.cpp
@@ -2,3 +2,27 @@
 Вернитесь к рассмотрению примера CIRCAREA главы. Напишите функцию с именем circarea(), которая аналогичным образом вычисляет площадь круга.
 Функция должна принимать один аргумент типа float и возвращать значение типа float. 
 Напишите функцию main(), которая просит пользователя ввести значение радиуса, вызывает функцию circarea(), а затем отображает результат вычисления на экране.*/
+
+
+#include <iostream>
+using namespace std;
+
+float circarea(float);
+
+int main()
+{setlocale(LC_ALL, ".1251"); system("color 0F"); srand(time(NULL));
+ 
+float r;
+cout << "Введите радиус окружности:";
+cin >> rr;
+cout << circarea(rr) << endl;
+
+ 
+system("pause"); return 0;
+}
+
+float circarea(float rad)
+{
+	const float PI = 3.14159F;
+	return PI * rad * rad;
+}

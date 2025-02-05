f23c8d9d78e21d240212d3700964ad884988c2b5
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-02-05 23:35:01 +0300

    Update 08.cpp

--- Chapter 5/08.cpp
+++ Chapter 5/08.cpp
@@ -1,1 +1,25 @@
+/*05_08ex.cpp
+Напишите функцию с именем swap(), обменивающую значениями два своих аргумента типа int (обратите внимание, что изменяться должны значения переменных из вызывающей программы,
+а не локальных переменных функции). Выберите способ передачи аргументов. Напишите вызывающую программу main(), использующую данную функцию.*/
 
+#include <iostream>
+using namespace std;
+
+void swap(int&, int&);
+
+int main()
+{setlocale(LC_ALL, ".1251"); system("color 0F"); srand(time(NULL));
+
+int x = 12, y = 6;
+swap(x, y);
+cout << "\nx=" << x << " y=" << y << endl;
+
+system("pause"); return 0;
+}
+
+void swap(int& a, int& b) 
+{ 
+  int temp = a; 
+  a = b; 
+  b = temp; 
+}

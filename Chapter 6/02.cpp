0b24bdc00642e07922337b603ce515480763041b
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-02-06 18:58:21 +0300

    Update 02.cpp

--- Chapter 6/02.cpp
+++ Chapter 6/02.cpp
@@ -21,7 +21,7 @@ private:
   unsigned int avto; 
   double sum;
 public:
-	tollBoth() : avto(0), sum(0.0) {}
+tollBoth() : avto(0), sum(0.0) {}
 void payingCar() 
 { 
   avto++; 
@@ -44,11 +44,11 @@ char ch;
 tollBoth booth1;
 cout << "Нажмите 0 для машины без оплаты," << "\nНажмите 1 для каждой оплачиваемой машины," << "\nEsc для выхода." << endl;
 do {
-	ch = getche();
-	if (ch == '0') 
-    booth1.nopayCar();
-	if (ch == '1') 
-    booth1.payingCar();
+ch = getche();
+if (ch == '0') 
+	booth1.nopayCar();
+if (ch == '1') 
+	booth1.payingCar();
 } while (ch != ESC);
 booth1.display();
 			

4c37faf0384d1d18c4f9b92341bdb995aba33196
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-02-09 23:18:39 +0300

    Update 01.cpp

--- Chapter 7/01.cpp
+++ Chapter 7/01.cpp
@@ -22,16 +22,17 @@ cout << endl; system("pause"); return 0;
 
 void reversit(char s[]) 
 {
-	int len = strlen(s) - 1;
-	for (int j = 0; j < len; j++) 
-  {
-		char y = s[j]; 
-    s[j] = s[len]; 
-    s[len--] = y;
-		//2 variant
-    //char y=s[j]; 
-    s[j] = s[len-j-1]; 
-    s[len-j-1] = y; 
-  }
+int len = strlen(s) - 1;
+for (int j = 0; j < len; j++) 
+{
+	char y = s[j]; 
+	s[j] = s[len]; 
+	s[len--] = y;
+	
+	//2 variant
+	//char y=s[j]; 
+	//s[j] = s[len-j-1]; 
+	//s[len-j-1] = y; 
+}
 	cout << "\nРезультат:\n" << s;
 }

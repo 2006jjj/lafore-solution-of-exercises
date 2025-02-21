7243fab433a55457880a33e20fe05c60a4b68b04
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-02-21 21:26:34 +0300

    Create 01.cpp

---
+++ Chapter_14/01.cpp
@@ -0,0 +1,32 @@
+/*14_01ex.cpp
+Напишите шаблон функции, возвращающей среднее арифметическое всех элементов массива. Аргументами функции должны быть имя и размер массива (типа int).
+В main() проверьте работу функции с массивами типа int, long, double и char.*/
+
+
+#include <iostream>
+using namespace std;
+
+template <class T>
+T average(T* array, const unsigned int size)
+{
+  double res = 0.0;
+  for (int j = 0; j < size; j++)
+    res += array[j];
+  return res / size;
+}
+
+int main(int argc, char* argv[])
+{setlocale(LC_ALL, ".1251"); system("color 0F"); srand(time(NULL));
+
+const unsigned int SZ = 5;
+int intA[SZ] = { 1,2,3,4,5 };
+cout << "\n\n\nAVERAGE INT ARRAY = " << average(intA, SZ) << endl;
+double doubleA[SZ] = { 1.4, 2.3, 6.4, 6.2, 8.4 };
+cout << "AVERAGE DOUBLE ARRAY = " << average(doubleA, SZ) << endl;
+long longA[SZ] = { 45L, 123L, 677L, 777L, 3434L };
+cout << "AVERAGE LONG ARRAY = " << average(longA, SZ) << endl;
+char charA[SZ] = { "NIKE" };
+cout << "AVERAGE CHAR ARRAY = " << average(charA, SZ) << " code of this symbol=" << (int)average(charA, SZ) << endl; 
+
+cout << endl; system("pause"); return 0;
+}

8986336dacc2f16944d2b73660ba5366b5b291ac
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-02-21 22:19:37 +0300

    Create 05.cpp

---
+++ Chapter_14/05.cpp
@@ -0,0 +1,34 @@
+/*14_05ex.cpp
+Создайте функцию amax(), возвращающую значение максимального элемента массива. Аргументами функции должны быть адрес и размер массива.
+Сделайте из функции шаблон, чтобы она могла работать с массивом любого числового типа. Напишите секцию main(), в которой проверьте работу функции с разными типами массивов.*/
+
+#include <iostream>
+using namespace std;
+
+
+template<class F>
+F amax(F* array, const unsigned int size)
+{
+  F res = array[0];
+  for (int j = 0; j < size; j++)
+  {
+    cout << array[j] << ' ';
+    if (res < array[j])
+      res = array[j];
+  }
+  cout << "\nMax Element=" << res;
+  return res;
+}
+
+int main()
+{setlocale(LC_ALL, ".1251"); system("color 0F"); srand(time(NULL));
+
+const unsigned int SZ = 5;
+int intA[SZ] = { 6,22,3,4,5 };
+amax(intA, SZ); cout << endl;
+ 
+float floatA[SZ] = { 4.54, 7.54, 88.88, 23.33, 1.23 };
+amax(floatA, SZ); 
+ 
+cout << endl; system("pause"); return 0;
+}

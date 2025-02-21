81a6b77dfe0c7c0753b32fc025ef9f7dcfd45e1f
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-02-21 21:28:01 +0300

    Update 01.cpp

--- Chapter_14/01.cpp
+++ Chapter_14/01.cpp
@@ -15,16 +15,19 @@ T average(T* array, const unsigned int size)
   return res / size;
 }
 
-int main(int argc, char* argv[])
+int main()
 {setlocale(LC_ALL, ".1251"); system("color 0F"); srand(time(NULL));
 
 const unsigned int SZ = 5;
 int intA[SZ] = { 1,2,3,4,5 };
 cout << "\n\n\nAVERAGE INT ARRAY = " << average(intA, SZ) << endl;
+ 
 double doubleA[SZ] = { 1.4, 2.3, 6.4, 6.2, 8.4 };
 cout << "AVERAGE DOUBLE ARRAY = " << average(doubleA, SZ) << endl;
+ 
 long longA[SZ] = { 45L, 123L, 677L, 777L, 3434L };
 cout << "AVERAGE LONG ARRAY = " << average(longA, SZ) << endl;
+ 
 char charA[SZ] = { "NIKE" };
 cout << "AVERAGE CHAR ARRAY = " << average(charA, SZ) << " code of this symbol=" << (int)average(charA, SZ) << endl; 
 

dd5aaeac052e810401ea04676f6083895b965537
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-02-23 20:08:46 +0300

    Update 03.cpp

--- Chapter_15/03.cpp
+++ Chapter_15/03.cpp
@@ -17,14 +17,17 @@ const int SZ = 10;
 int arr[SZ] = { 2,-4,6,-8,10,-12,14,-16,18,-20 };
 list<int> intlist(arr, arr + SZ);
 int size = intlist.size();
+ 
 list<int>::iterator iter1, iter2;
 iter1 = intlist.begin();
 iter2 = intlist.end();
+ 
 while (iter1 != iter2)
   swap(*iter1++, *--iter2);
- cout << endl;
- for (iter2 = intlist.begin(); iter2 != intlist.end(); )
-   cout << *iter2++ << ' '; 
+cout << endl;
+ 
+for (iter2 = intlist.begin(); iter2 != intlist.end(); )
+  cout << *iter2++ << ' '; 
 
 cout << endl; system("pause"); return 0;
 }

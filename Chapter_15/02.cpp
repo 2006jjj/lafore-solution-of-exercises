eb6dc36cfab370726531bbf04d8fcb9b3e102f1e
死 yohji <47281780+2006jjj@users.noreply.github.com>
2025-02-23 19:34:42 +0300

    Create 02.cpp

---
+++ Chapter_15/02.cpp
@@ -0,0 +1,31 @@
+/*Примените алгоритм sort() к массиву слов, введенных пользователем, выведите результат. Используйте push_back() для добавления слов,
+а оператор [] и size() - для вывода их на экран.*/
+
+#include <iostream>
+#include <algorithm> 
+#include <vector>
+#include <string> 
+using namespace std;
+
+int main()
+{setlocale(LC_ALL, ".1251"); system("color 0F"); srand(time(NULL));
+
+vector<string> names;
+char ch;
+string name;
+do
+{
+  cout << "Введите имя:"; 
+  cin >> name;
+  names.push_back(name);
+  cout << "Желаете продолжить? (y/n):";
+  cin >> ch;
+} while (ch != 'n');
+
+sort(names.begin(), names.end());
+int size = names.size();
+for (int j = 0; j < names.size(); j++)
+  cout << names[j] << "\n"; 
+
+cout << endl; system("pause"); return 0;
+}
